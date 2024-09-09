9a58babe4e1e1de4d9969e83e5452a51f3834e84
HazemAlShibani <hazem.alshibani03@gmail.com>
2024-09-09 18:33:23 +0200

    comme on

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,23 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: "/api/:path*",
-        destination: "https://casecobra-amber-one.vercel.app/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Or specify your allowed origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
       },
     ];
   },
