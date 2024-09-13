ed492fe8d3bb72f7d7adb2149c1fad2776aab25d
HazemAlShibani <hazem.alshibani03@gmail.com>
2024-09-13 16:23:12 +0200

    other updates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -43,6 +43,6 @@ export function constructMetadata({
       creator: '@hazemAlShibani',
     },
     icons,
-    metadataBase: new URL("https://casecobra-lime.vercel.app/")
+    metadataBase: new URL("https://casecobra-lime.vercel.apps")
   }
 }
