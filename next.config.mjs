8a09d497e104a71aaf97a393cf59d0fd53be731d
HazemAlShibani <hazem.alshibani03@gmail.com>
2024-09-13 18:54:48 +0200

    other updates

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,26 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Change this to specific domain for better security
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
